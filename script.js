19a81892cab0a2f293799437942f8bc0bb904c9e
RenanLMenezes <renanlmenezes2003@gmail.com>
2021-08-10 10:20:08 -0300

    Add Js Html Css

---
+++ script.js
@@ -0,0 +1,111 @@
+function button(){
+    document.getElementById("click").innerHTML = "apertou"
+    
+    //alert("Apertou")
+}
+
+function red(){
+    window.open("https://www.crunchyroll.com/pt-br/videos/anime")
+    //window.location.href = "https://www.crunchyroll.com/pt-br/videos/anime"
+}
+
+function change(elemento){
+    //document.getElementById("mouseChange").innerHTML = "olha lá"
+   elemento.innerHTML = "olha lá"
+}
+
+function voltar(elemento){
+    //document.getElementById("mouseChange").innerHTML = "Passe mouse aqui"
+    elemento.innerHTML = "Passe mouse aqui"
+}
+
+function load(){
+    alert("pag load")
+}
+
+function funcChange(elemento){
+    console.log(elemento.value)
+}
+
+//Quarta Aula
+/*function soma(n1, n2){
+    return n1 + n2
+}
+
+//var val global
+function valAge(age){
+    var val //local
+    if(age >= 18){
+        val = true
+    }
+    else{
+        val = false
+    }
+    return val    
+}
+
+var age = prompt("idade")
+console.log(valAge(age))
+
+function setReplace(frase, name, newName){
+    return frase.replace(name, newName)
+}
+
+alert(soma(1, 10))
+alert(setReplace("Salve, dibas", "dibas", " suave"))
+
+//Terceira Aula
+var d = new Date();
+console.log(d.getMonth()+1 + " month")
+console.log(d.getDay()+ " day")
+console.log(d.getMilliseconds() + " milliseconds")
+console.log(d.getMinutes() + " minutes")
+console.log(d.getSeconds() + " seconds")
+var count;
+for(count=0; count<=5; count++){
+    console.log(count)
+}
+
+var count = 0
+while(count <= 5){
+    console.log(count)
+    count++
+}
+
+var idade = prompt ("qual sua idade");
+//var idade = 18
+if (idade >= 18){
+    alert("maior de idade")
+}
+else{
+    alert("menor de idade")
+}
+
+
+
+//Segunda Aula
+var bebidas = [{nome:"matinal", cor:"escuro", intensidade:"forte"}, 
+{nome:"Agua", cor:"claro", intensidade:"leve"}]
+console.log(bebidas)*/
+/*var café = {nome:"matinal", cor:"escuro", intensidade:"forte"} //dictionary
+console.log(café)
+var lista = ["café","café2","café3"]
+lista.push("cafe4") //por um item
+//lista.pop()// tirar o ultimo da lista
+console.log(lista.length)//tamanho
+console.log(lista.reverse())
+console.log(lista.toString())
+console.log(lista.join(" - "))
+
+
+
+//Primeira aula
+var nome = "Renan Menezes"
+var n1 = 18
+var n2 = 21
+var oi = "oi"
+alert(nome + " tem " + idade + " anos ")
+alert(n1 * n2)
+console.log(nome.lowerCase/upperCase)
+console.log(n1 + n2)
+console.log(oi.replace("oi", "ola"))*/
